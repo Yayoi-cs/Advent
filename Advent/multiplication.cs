ea4791d9c0c4282e4e96fb4c17c7d48528b33711
yayoi #03 <99739136+hirogoshawk@users.noreply.github.com>
2022-12-08 23:00:53 +0900

    プロジェクト ファイルを追加します。

---
+++ Advent/multiplication.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Advent
+{
+    public class multiplication
+    {
+        public static int Remainder(int array1, int array2, int array3)
+        {
+            int Array3_d = array1;
+            for (int i = 0; i < array2 - 1; i++)
+            {
+                Array3_d = ((Array3_d % array3) * array1);
+            }
+            Array3_d = Array3_d % array3;
+            return Array3_d;
+        }
+    }
+}
